c204bb3f78537929f9aab499236abda91938eb9e
evanyans <48900125+evanyans@users.noreply.github.com>
2020-08-13 13:19:49 -0700

    wo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreadDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreadDemo.java
@@ -1,25 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.ThreadPool;
 
 import java.util.concurrent.ExecutorService;
 
-@Autonomous(name="Gyro Demo", group="Demos")
+@TeleOp(name="Thread Demo", group="Demos")
 
 public class ThreadDemo extends LinearOpMode {
-    private GyroBot robot = new GyroBot(this);
+    private ThreadDemo robot = new ThreadDemo(this);
     private ExecutorService updateExecutor;
     private AnotherThreadTest whatever = new AnotherThreadTest();
-
-    @Override
-    public void runOpMode() {
+    DcMotor verticalLeft, verticalRight, horizontal;
+    BNO055IMU imu;
+    String rfName = "rf", rbName = "rb", lfName = "lf", lbName = "lb";
+    String verticalLeftEncoder = rbName, verticalRightEncoder = lfName, horizontalEncoder = rfName;
+    final double PIVOT_SPEED = 0.5;
+    final double COUNTS_PER_INCH = 307.699557;
+    ElapsedTime timer = new ElapsedTime();
+    double horizontalTickOffset = 0;
+@Override
+        public void runOpMode() {
         robot.init(hardwareMap);
         updateExecutor = ThreadPool.newSingleThreadExecutor("update");
         waitForStart();
         updateExecutor.submit(whatever);
         while (opModeIsActive()) {
+            double changeRight = robot.verticalRight.getCurrentPosition() - lastRightCount;
+            double changeLeft = -robot.verticalLeft.getCurrentPosition() - lastLeftCount;
+            double changeBack = robot.horizontal.getCurrentPosition() - lastBackCount;
 
         }
     }
