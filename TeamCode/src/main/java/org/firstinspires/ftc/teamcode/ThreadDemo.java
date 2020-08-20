994508af6f3dabcaf9de32cdc80772d8f7affca7
gronkybong <48972370+gronkybong@users.noreply.github.com>
2020-08-20 15:13:40 -0700

    working multithreading?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreadDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreadDemo.java
@@ -10,25 +10,29 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @Autonomous(name="Thread Demo", group="Demos")
 
 public class ThreadDemo extends LinearOpMode {
+
+
     public DcMotor horizontal = null;
+    public DcMotor verticalLeft = null;
+    public DcMotor verticalRight = null;
 
     String rfName = "rf", rbName = "rb", lfName = "lf", lbName = "lb";
     String verticalLeftEncoderName = rbName, verticalRightEncoderName = lfName, horizontalEncoderName = rfName;
+
+    double xCurrentBlue = 0, yCurrentBlue = 0, thetaCurrentBlue = 60;
+    double xRed, yRed;
+    double xChangeBlue, yChangeBlue;
 
     @Override
         public void runOpMode() {
         initDriveHardwareMap(rfName, rbName, lfName, lbName, verticalLeftEncoderName, verticalRightEncoderName, horizontalEncoderName);
         telemetry.addData("Status", "Init Complete");
         telemetry.update();
+        Thread caseThreeThread = new CaseThree();
         waitForStart();
-        Thread positionThread = new Thread();
-        positionThread.start();
+        caseThreeThread.start();
         while (opModeIsActive()) {
-
-        telemetry.addData("horizontal encoder position", horizontal.getCurrentPosition());
-
-        telemetry.addData("Thread Active", positionThread.isAlive());
-        telemetry.update();
+            telemetry.addData(String.format("Position: %2d, %2d", xCurrentBlue, yCurrentBlue));
     }
 }
     private void initDriveHardwareMap(String rfName, String rbName, String lfName, String lbName, String vl, String vr, String h){
@@ -36,9 +40,36 @@ public class ThreadDemo extends LinearOpMode {
         horizontal = hardwareMap.dcMotor.get("h");
         horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        verticalLeft = hardwareMap.dcMotor.get("v1");
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalRight = hardwareMap.dcMotor.get("v2");
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         telemetry.addData("Status", "Hardware Map Init Complete");
         telemetry.update();
     }
+
+
+
+    public class CaseThree extends Thread {
+
+        private void calculateCaseThree() {
+            xRed = (verticalLeft.getCurrentPosition() + verticalRight.getCurrentPosition())/2;
+            yRed = horizontal.getCurrentPosition();
+
+            xChangeBlue = Math.sin(thetaCurrentBlue)*yRed + Math.cos(thetaCurrentBlue)*xRed;
+            yChangeBlue = Math.sin(thetaCurrentBlue)*xRed + Math.cos((thetaCurrentBlue)*yRed);
+
+            xCurrentBlue =+ xChangeBlue;
+            yCurrentBlue =+ xChangeBlue;
+        }
+
+        public void run() {
+            while (true) {
+                calculateCaseThree();
+            }
+        }
     }
+}
