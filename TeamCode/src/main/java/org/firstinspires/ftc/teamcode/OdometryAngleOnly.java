3ee22bf8445e93f0ff3d6929870e6436fe181a1d
evanyans <48900125+evanyans@users.noreply.github.com>
2020-08-26 10:32:25 -0700

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAngleOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryAngleOnly.java
@@ -5,29 +5,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="Odometry Angle Only", group="Demos")
 public class OdometryAngleOnly implements Runnable {
-    //Odometry wheels
     private DcMotor verticalEncoderLeft, verticalEncoderRight, horizontalEncoder;
 
-    //Thead run condition
     private boolean isRunning = true;
 
-    //Position variables for storage
     double verticalRightEncoderWheelPosition = 0, verticalLeftEncoderWheelPosition = 0, changeInRobotOrientation = 0;
+    double verticalRightEncoderWheelPosition = 0, verticalLeftEncoderWheelPosition = 0, angleChange = 0;
     private double robotOrientationRadians = 0;
     private double previousVerticalRightEncoderWheelPosition = 0, previousVerticalLeftEncoderWheelPosition = 0;
 
-    //this needs to be calculated
-    private double robotEncoderWheelDistance;
-
+    //how to find this
+    private double radius;
+    public OdometryAngleOnly(DcMotor verticalEncoderLeft, DcMotor verticalEncoderRight, DcMotor horizontalEncoder) {
+        this.verticalEncoderLeft = verticalEncoderLeft;
+        this.verticalEncoderRight = verticalEncoderRight;
+        this.horizontalEncoder = horizontalEncoder;
+    }
     private void globalCoordinatePositionUpdate(){
-        //getting current positions
         verticalLeftEncoderWheelPosition = verticalEncoderLeft.getCurrentPosition();
         verticalRightEncoderWheelPosition = verticalEncoderRight.getCurrentPosition();
         double leftChange = verticalLeftEncoderWheelPosition - previousVerticalLeftEncoderWheelPosition;
         double rightChange = verticalRightEncoderWheelPosition - previousVerticalRightEncoderWheelPosition;
-        //calculating the angle
-        changeInRobotOrientation = (leftChange - rightChange) / Math.PI * (robotEncoderWheelDistance);
-        robotOrientationRadians = ((robotOrientationRadians + changeInRobotOrientation));
+        angleChange = (leftChange - rightChange) / Math.PI * (radius);
+        robotOrientationRadians = ((angleChange + robotOrientationRadians));
     }
     @Override
     public void run() {
