7c6aa7e35a73a5cc622e55cf72786f0f8d8a57e9
gronkybong <48972370+gronkybong@users.noreply.github.com>
2020-10-15 14:39:16 -0700

    led test and works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -1,13 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.robot.Robot;
-import com.qualcomm.robotcore.util.RobotLog;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
 
 @Autonomous(name="Camera Test", group="Tests")
 
